20d7e55ae44f07b85fec281c922e77e6fb9c0682
blkclct <paradox.h129@gmail.com>
2020-03-04 01:47:30 +0900

    Update: Enterキーを押されてエラーになっていたのを修正した

--- src/main.rs
+++ src/main.rs
@@ -18,8 +18,10 @@ fn main() {
         io::stdin().read_line(&mut guess_number)
             .expect("Failed to read line");
     
-        let guess_number: u32 = guess_number.trim().parse()
-        .expect("Please type a number.");
+        let guess_number: u32 = match guess_number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
     
         println!("You guessed: {}", guess_number);
     
