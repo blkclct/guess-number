1f4d87299438c384ea7a17f39dab8851649f298e
blkclct <paradox.h129@gmail.com>
2020-03-04 01:28:59 +0900

    Update: 秘密の数を生成する処理を加えた

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("数字を推測しよう！");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("秘密の数: {}", secret_number);
 
     println!("推測する数を入力する．");
     let mut guess_number = String::new();
