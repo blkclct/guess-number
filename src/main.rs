35b0b574fe12ed61f3b53708e67960242c845c90
blkclct <paradox.h129@gmail.com>
2020-03-04 01:45:25 +0900

    Update: 推測値の入力をループで促す処理を入れた，また一致したら終了する処理を入れた

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,25 @@ fn main() {
 
     println!("秘密の数: {}", secret_number);
 
-    println!("推測する数を入力する．");
-    let mut guess_number = String::new();
-
-    io::stdin().read_line(&mut guess_number)
-        .expect("Failed to read line");
-
-    let guess_number: u32 = guess_number.trim().parse()
-    .expect("Please type a number.");
-
-    println!("You guessed: {}", guess_number);
-
-    match guess_number.cmp(&secret_number) {
-        Ordering::Less => println!("数が小さすぎます！"),
-        Ordering::Greater => println!("数が大きすぎます！"),
-        Ordering::Equal => println!("一致しました！"),
+    loop {
+        println!("推測する数を入力する．");
+        let mut guess_number = String::new();
+    
+        io::stdin().read_line(&mut guess_number)
+            .expect("Failed to read line");
+    
+        let guess_number: u32 = guess_number.trim().parse()
+        .expect("Please type a number.");
+    
+        println!("You guessed: {}", guess_number);
+    
+        match guess_number.cmp(&secret_number) {
+            Ordering::Less => println!("数が小さすぎます！"),
+            Ordering::Greater => println!("数が大きすぎます！"),
+            Ordering::Equal => {
+                println!("一致しました！");
+                break;
+            },
+        }
     }
 }
